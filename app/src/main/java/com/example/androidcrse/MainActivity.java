da62ffb4b27e2f9cfc714f190d9592775a3c7ad6
DivyaSangaraju <divyacse1234@gmail.com>
2020-12-22 12:45:38 +0530

    first commit

---
+++ app/src/main/java/com/example/androidcrse/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidcrse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void btnclick(View view) {
+        Intent i = new Intent(MainActivity.this,CustomNavigation.class);
+        startActivity(i);
+    }
+}
